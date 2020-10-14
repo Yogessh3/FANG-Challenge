7167eecf906bc04c869014c80b0ab04baa688089
YOGESSH V <40272613+Yogessh3@users.noreply.github.com>
2020-10-14 11:11:42 +0530

    Update KMPAlgorithm_FamousAlgorithms.py

--- KMPAlgorithm_FamousAlgorithms.py
+++ KMPAlgorithm_FamousAlgorithms.py
@@ -8,6 +8,7 @@ def buildPattern(substring):
     pattern=[-1 for i in substring]
     while i<len(substring):
         if(substring[i]==substring[j]):
+            pattern[i]=j
             i+=1
             j+=1
         elif(j>0):
@@ -18,7 +19,7 @@ def buildPattern(substring):
 def matchPattern(string,substring,pattern):
     i=0
     j=0
-    while i +len(substring) - j < len(string):
+    while i +len(substring) - j <= len(string):
         if(string[i]==substring[j]):
             if(j==len(substring)-1):
                 return True
